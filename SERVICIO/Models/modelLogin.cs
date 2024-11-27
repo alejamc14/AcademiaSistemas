2ede1272e5dd96f59c35b1216253d9850fcb6443
Cristian Fernando Cardona Agudelo <cristianfernando.c@hotmail.com>
2024-11-26 21:13:33 -0500

    Modellogin(Para el login).

---
+++ SERVICIO/Models/modelLogin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SERVICIO.Models
+{
+    public class Login
+    {
+        public string Usuario { get; set; }
+        public string Clave { get; set; }
+    }
+    public class LoginRespuesta
+    {
+        public string Usuario { get; set; }
+        public string Perfil { get; set; }
+        public string Token { get; set; }
+        public bool Autenticado { get; set; }
+        public string PaginaInicio { get; set; }
+        public string Mensaje { get; set; }
+    }
+}
